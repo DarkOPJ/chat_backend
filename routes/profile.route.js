9c121c8d81b4440e318f2a6d36622a1a829896a3
Boadu Aaron Jeffrey Bosomepem <100956199+DarkOPJ@users.noreply.github.com>
2025-10-21 23:58:39 +0000

    fix: Add protected_route middleware to update profile route

--- routes/profile.route.js
+++ routes/profile.route.js
@@ -1,8 +1,9 @@
 import express  from "express";
 import { update_profile } from "../controllers/profile.controller.js";
+import protected_route from "../middleware/protected_route.js";
 
 const router = express.Router()
 
-router.get("/update", update_profile)
+router.get("/update", protected_route, update_profile)
 
 export default router
